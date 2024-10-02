e9368969239244db9a262e2427b7cc9c136e68c0
noby_y <noby.official@gmail.com>
2024-10-02 19:28:56 +0300

    fix: removed tables with unused mods

--- src/infini_wisp_calc.cpp
+++ src/infini_wisp_calc.cpp
@@ -252,7 +252,7 @@ int fill_table(fort::utf8_table& table){
     for (auto sol : solution_list){
         table << sol.mod_used;
         for (int i = 0; i < mod_count; ++i){
-            table << sol.mods[i];
+            if (coef[i] != 0) table << sol.mods[i];
         }
         table << fort::endr;
     }
